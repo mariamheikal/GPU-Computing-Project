584db2446ca5684fb844f74dd4cbc9d133c2d316
Mariam Heikal <mariamheikal228@gmail.com>
2022-04-27 00:11:53 +0300

    Update kernel2.cu

--- kernel2.cu
+++ kernel2.cu
@@ -1,11 +1,120 @@
-
 #include "common.h"
 #include "timer.h"
+#define VERTICES_PER_BLOCK 32
+#define NEIGHBORS_of_NIEGHBORS_s 6140
+#define BLOCK_DIM 1024
+#define THRESHOLD 80000
+#define WARP_SIZE 32
+__global__ void jaccard_gpu2_kernel(CSRGraph* csrGraph_d, COOMatrix* cooMatrix_d, unsigned int* numCommonNeighbors, unsigned int* neighborsOfNeighbors){
+    __shared__ unsigned int numNeighborsOfNeighbors;
+    __shared__ unsigned int numNeighborsOfNeighbors_s;
+    __shared__ unsigned int numCommonNeighbors_s[NEIGHBORS_of_NIEGHBORS_s];
+    __shared__ unsigned int neighborsOfNeighbors_s[NEIGHBORS_of_NIEGHBORS_s];
+    unsigned int arrStartIndx = blockIdx.x*(csrGraph_d->numVertices-NEIGHBORS_of_NIEGHBORS_s);
+
+    for(unsigned int i=0; i<NEIGHBORS_of_NIEGHBORS_s; i+=blockDim.x){
+        numCommonNeighbors_s[i+threadIdx.x]=0;
+        neighborsOfNeighbors_s[i+threadIdx.x]=0;
+    }
+    if(threadIdx.x==0) {
+        numNeighborsOfNeighbors=0;
+        numNeighborsOfNeighbors_s=0;
+    }
+    __syncthreads();
+       unsigned int numVerticesPerBlock= VERTICES_PER_BLOCK*(1+(csrGraph_d->numVertices)/THRESHOLD);
+
+       for(unsigned int v=0; v<numVerticesPerBlock; ++v){
+           unsigned int vertex = numVerticesPerBlock*blockIdx.x+v;
+
+           if(vertex< csrGraph_d->numVertices){
+                for(unsigned int e=csrGraph_d->srcPtrs[vertex]; e<csrGraph_d->srcPtrs[vertex + 1]; e+=blockDim.x/WARP_SIZE){
+
+                        unsigned int edge = e+threadIdx.x/WARP_SIZE;
+                        if(edge<csrGraph_d->srcPtrs[vertex + 1]){
+
+                            unsigned int neighbor = csrGraph_d->dst[edge];
+
+                            for(long neighborEdge = csrGraph_d->srcPtrs[neighbor + 1]; neighborEdge > (long) csrGraph_d->srcPtrs[neighbor]; neighborEdge-=WARP_SIZE) {
+                                if (neighborEdge - (threadIdx.x % WARP_SIZE) - 1 > csrGraph_d->srcPtrs[neighbor]){
+                                        unsigned int neighborOfNeighbor = csrGraph_d->dst[neighborEdge - 1 -threadIdx.x%WARP_SIZE];
+						                            if(neighborOfNeighbor > vertex) {
+                                            if(neighborOfNeighbor<vertex+NEIGHBORS_of_NIEGHBORS_s+1){
+                                                unsigned int oldVal = atomicAdd(&numCommonNeighbors_s[neighborOfNeighbor-vertex-1],1);
+                                                if(oldVal == 0) {
+                                                    neighborsOfNeighbors_s[atomicAdd(&numNeighborsOfNeighbors_s,1)] = neighborOfNeighbor;
+                                                }
+                                            }
+                                            else{
+                                                unsigned int oldVal = atomicAdd(&numCommonNeighbors[arrStartIndx+neighborOfNeighbor-NEIGHBORS_of_NIEGHBORS_s],1);
+                                                if(oldVal == 0) {
+                                                    neighborsOfNeighbors[arrStartIndx+atomicAdd(&numNeighborsOfNeighbors,1)] = neighborOfNeighbor;
+                                                }
+                                            }
+                                        } else {
+                                            break;
+                                        }
+                                }
+                            }
+                        }
+
+                }
+                __syncthreads();
+
+                for(unsigned int i=0; i<numNeighborsOfNeighbors_s; i+=blockDim.x){
+
+                    if(i+threadIdx.x<numNeighborsOfNeighbors_s && i+threadIdx.x<NEIGHBORS_of_NIEGHBORS_s){
+                        unsigned int vertex2 = neighborsOfNeighbors_s[i+threadIdx.x];
+                        if(numCommonNeighbors_s[vertex2-vertex-1] > 0) {
+                            unsigned int numNeighbors = csrGraph_d->srcPtrs[vertex + 1] - csrGraph_d->srcPtrs[vertex];
+                            unsigned int numNeighbors2 = csrGraph_d->srcPtrs[vertex2 + 1] - csrGraph_d->srcPtrs[vertex2];
+                            float jaccardSimilarity = ((float) numCommonNeighbors_s[vertex2-vertex-1])/(numNeighbors + numNeighbors2 - numCommonNeighbors_s[vertex2-vertex-1]);
+                            unsigned int j = atomicAdd(&cooMatrix_d->nnz,1);
+                            cooMatrix_d->rowIdxs[j] = vertex;
+                            cooMatrix_d->colIdxs[j] = vertex2;
+                            cooMatrix_d->values[j] = jaccardSimilarity;
+                            numCommonNeighbors_s[vertex2-vertex-1] = 0;
+                        }
+                    }
+                }
+
+
+                for(unsigned int i=0; i<numNeighborsOfNeighbors; i+=blockDim.x){
+
+                    if(i+threadIdx.x<numNeighborsOfNeighbors){
+                        unsigned int vertex2 = neighborsOfNeighbors[arrStartIndx+i+threadIdx.x];
+                        if(numCommonNeighbors[arrStartIndx+vertex2-NEIGHBORS_of_NIEGHBORS_s] > 0) {
+                            unsigned int numNeighbors = csrGraph_d->srcPtrs[vertex + 1] - csrGraph_d->srcPtrs[vertex];
+                            unsigned int numNeighbors2 = csrGraph_d->srcPtrs[vertex2 + 1] - csrGraph_d->srcPtrs[vertex2];
+                            float jaccardSimilarity = ((float) numCommonNeighbors[arrStartIndx+vertex2-NEIGHBORS_of_NIEGHBORS_s])/(numNeighbors + numNeighbors2 - numCommonNeighbors[arrStartIndx+vertex2-NEIGHBORS_of_NIEGHBORS_s]);
+                            unsigned int j = atomicAdd(&cooMatrix_d->nnz,1);
+                            cooMatrix_d->rowIdxs[j] = vertex;
+                            cooMatrix_d->colIdxs[j] = vertex2;
+                            cooMatrix_d->values[j] = jaccardSimilarity;
+                            numCommonNeighbors[arrStartIndx+vertex2-NEIGHBORS_of_NIEGHBORS_s] = 0;
+                        }
+                    }
+                }
+            }
+            __syncthreads();
+            if(threadIdx.x==0) {numNeighborsOfNeighbors=0; numNeighborsOfNeighbors_s=0;}
+        }
+    }
 
 void jaccard_gpu2(CSRGraph* csrGraph, CSRGraph* csrGraph_d, COOMatrix* cooMatrix_d) {
 
+    unsigned int numThreadsPerBlock = BLOCK_DIM;
+    unsigned int numVertices = csrGraph->numVertices;
+    unsigned int numBlocks = (numVertices+VERTICES_PER_BLOCK*(1+numVertices/THRESHOLD)-1)/(VERTICES_PER_BLOCK*(1+numVertices/THRESHOLD));
+    unsigned int* numCommonNeighbors;
+    unsigned int* neighborsOfNeighbors;
+    cudaMalloc((void**) &numCommonNeighbors, numBlocks*(numVertices-NEIGHBORS_of_NIEGHBORS_s)*sizeof(unsigned int));
+    cudaMalloc((void**) &neighborsOfNeighbors, numBlocks*(numVertices-NEIGHBORS_of_NIEGHBORS_s)*sizeof(unsigned int));
+    cudaMemset(numCommonNeighbors, 0, numBlocks*(numVertices-NEIGHBORS_of_NIEGHBORS_s)*sizeof(unsigned int));
+    cudaMemset(neighborsOfNeighbors, 0, numBlocks*(numVertices-NEIGHBORS_of_NIEGHBORS_s)*sizeof(unsigned int));
+    cudaDeviceSynchronize();
 
+    jaccard_gpu2_kernel<<<numBlocks, numThreadsPerBlock>>>(csrGraph_d, cooMatrix_d, numCommonNeighbors, neighborsOfNeighbors);
 
-
+    cudaFree(numCommonNeighbors);
+    cudaFree(neighborsOfNeighbors);
 }
-
